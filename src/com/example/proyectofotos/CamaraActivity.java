83145636b2d3c916a82be571669bd31b645ff99a
Mauro Bahamondes <maurobahamondes@gmail.com>
2013-02-01 19:20:52 -0300

    image

--- src/com/example/proyectofotos/CamaraActivity.java
+++ src/com/example/proyectofotos/CamaraActivity.java
@@ -1,15 +1,43 @@
 package com.example.proyectofotos;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class CamaraActivity extends Activity {
-
+	
+	private String foto = "";
+	private static int TOMAR_FOTO = 1;
+	private static int SELECCIONAR_FOTO = 2;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camara);
+		
+		//asignar nombre a foto
+		foto = Environment.getExternalStorageDirectory() + "/test.jpg";
+		
+		//Mapear los elementos
+		RadioButton rbtnFoto = (RadioButton) this.findViewById(R.id.btnFoto);
+		RadioButton rbtnGaleria = (RadioButton) this.findViewById(R.id.btnGaleria);
+		
+		Button btnCapturar = (Button) this.findViewById(R.id.btnCapturar);
+		
+		btnCapturar.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
 
 	@Override
